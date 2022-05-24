c7d5d6b3b3f75a9d6a0d2474b5a2bd88a10c3237
lisagent <lisa.gent1@googlemail.com>
2022-05-24 20:01:11 +0200

    StateMachine überarbeitet, neu im fillBuffer

--- QAMDecGen/qamgen.c
+++ QAMDecGen/qamgen.c
@@ -23,12 +23,10 @@
 #include "qaminit.h"
 #include "qamgen.h"
 
+//void Interface(void *pvParameters);
+
 #define MODE_IDLE		0x00
-#define MODE_START		0x01
-#define MODE_ID			0x02
-#define MODE_LENGHT		0x03
-#define MODE_DATA		0x04
-#define MODE_CHECKSUMME	0x05
+#define MODE_SENDING	0x01
 
 
 const int16_t sinLookup100[NR_OF_SAMPLES*2] = {0x0,0x18F,0x30F,0x471,0x5A7,0x6A6,0x763,0x7D8,
@@ -49,15 +47,18 @@ const int16_t sinLookup50[NR_OF_SAMPLES*2] = {0x0,0xC8,0x187,0x238,0x2D3,0x353,0
 												0x0,0xFF38,0xFE79,0xFDC8,0xFD2D,0xFCAD,0xFC4F,0xFC15,
 												0xFC01,0xFC15,0xFC4F,0xFCAD,0xFD2D,0xFDC8,0xFE79,0xFF38,};
 
-#define SENDBUFFER_SIZE		 20
-#define SENDBUFFER_SIZE_1	 2
-#define SENDBUFFER_SIZE_2	 4
-#define SENDBUFFER_SIZE_3	 48
+#define SENDBUFFER_SIZE_IDLE		 2
+#define SENDBUFFER_SIZE_SENDING		 2
+
 uint8_t sendbuffer[SENDBUFFER_SIZE] = {0,1,0,1,0,1,2,1,3,0,1,1,3,2,1,0,0,1,0,1};
 
 //Es soll eine StateMachine erstellt werden, mit deren Hilfe die einzelnen States ablaufen. 
 //Sie soll aus folgenden State bestehen: Idle, Start, ID,Lenght,Data,Checksumme
-//
+//Stand 24.05.22 Die StateMAchine soll nicht so genau definiert sein. Es soll eine StateMachine im fillBuffer gemacht werden
+//Diese soll nur die zwei Zustände Idle und Sending haben. Im Sending werden alle Daten von Start,ID,Lengt,... zusammen geschrieben
+//Es muss noch einen Extratask haben, zb direkt im vQuamGen. Der besteht aus Taste und Encoder. Es kann die gleiche Tastenabfrage wie bei den anderen Projekten genommen werden
+//Wenn die Taste gedrückt wird, werden alle Daten durch den Encoder in den SendingBuffer geschrieben. Durch ein Semaphore soll dann noch sicher gestellt werden, dass die Daten
+//Nur im Idle-Zustand gesendet werden. Wenn er grade im Sending-State ist dürfen keine Daten übertragen werden.
 
 void vQuamGen(void *pvParameters) {
 	while(evDMAState == NULL) {
@@ -65,7 +66,29 @@ void vQuamGen(void *pvParameters) {
 	}
 	xEventGroupWaitBits(evDMAState, DMAGENREADY, false, true, portMAX_DELAY);
 	for(;;) {
-		vTaskDelay(10/portTICK_RATE_MS);
+			updateButtons();
+			
+			if(getButtonPress(BUTTON1) == SHORT_PRESSED) {
+			}
+			if(getButtonPress(BUTTON2) == SHORT_PRESSED) {
+			}
+			if(getButtonPress(BUTTON3) == SHORT_PRESSED) {
+			}
+			if(getButtonPress(BUTTON4) == SHORT_PRESSED) {
+			}
+			if(getButtonPress(BUTTON1) == LONG_PRESSED) {
+			}
+			if(getButtonPress(BUTTON2) == LONG_PRESSED) {
+			}
+			if(getButtonPress(BUTTON3) == LONG_PRESSED) {
+			}
+			if(getButtonPress(BUTTON4) == LONG_PRESSED) {
+			}
+			vTaskDelay(10/portTICK_RATE_MS);
+	}
+	//xTaskCreate(Interface, NULL, configMINIMAL_STACK_SIZE+500, NULL, 2, NULL);
+
+		}
 	}
 }
 
@@ -93,81 +116,90 @@ void fillBuffer(uint16_t buffer[NR_OF_SAMPLES]) {
 	} else {
 		pSendbuffer = 0;
 	}
-}
-
-void StateMachine(void *pvParameters){   //muss noch implementiert werden
-	int mode = MODE_IDLE;
 	for(;;){
 		switch(mode)
 			case MODE_IDLE:
-				uint8_t sendbuffer[SENDBUFFER_SIZE_1] = {0,3};
-				if (Button1 = 1 ) {
-					mode = MODE_IDLE;
-						if(pSendbuffer < SENDBUFFER_SIZE_1-1) {
-							pSendbuffer++;
-						} else {
-							pSendbuffer = 0;
-						}								
-				}
-					if (Button1 = 0) {
-					mode = MODE_START;
-						if(pSendbuffer < SENDBUFFER_SIZE_1-1) {
-							pSendbuffer++;
-						} else {
-							pSendbuffer = 0;
-						}						
-				}
+				uint8_t sendbuffer[SENDBUFFER_SIZE_IDLE] = {0,3};
 			break;
-			case MODE_START:
-				uint8_t sendbuffer[SENDBUFFER_SIZE_1] = {1,2};
-				if(pSendbuffer < SENDBUFFER_SIZE_1-1) {
-					pSendbuffer++;
-					mode = MODE_START;
-				} else {
-					pSendbuffer = 0;
-					mode = MODE_ID;
-				}		
-			break;			
-			case MODE_ID:
-				uint8_t sendbuffer[SENDBUFFER_SIZE_2] = {Daten, die im ID gesendet werden sollen. Sollen spaeter mal veraenderbar sein, um die Checksumme zu kontrollieren};
-				if(pSendbuffer < SENDBUFFER_SIZE_2-1) {
-					pSendbuffer++;
-					mode = MODE_ID;
-				} else {
-					pSendbuffer = 0;
-					mode = MODE_LENGHT;
-				}
+			case MODE_SENDING
+				uint8_t sendbuffer[SENDBUFFER_SIZE_IDLE] = {alle Daten zusammen};
 			break;
-			case MODE_LENGHT
-				uint8_t sendbuffer[SENDBUFFER_SIZE_2] = {0,0,2,3};
-				if(pSendbuffer < SENDBUFFER_SIZE_2-1) {
-					pSendbuffer++;
-					mode = MODE_LENGHT;
-				} else {
-					pSendbuffer = 0;
-					mode = MODE_DATA;
-				}					
-			case MODE_DATA:
-				uint8_t sendbuffer[SENDBUFFER_SIZE_3] = {1,1,0,1, 1,3,1,1, 1,2,0,1, 1,2,3,1, 0,2,0,0, 1,1,0,0, 1,2,3,0, 1,3,1,1, 1,2,3,2, 1,2,1,0, 1,2,1,1, 1,3,0,2};	
-				if(pSendbuffer < SENDBUFFER_SIZE_2-1) {
-					pSendbuffer++;
-					mode = MODE_DATA;
-				} else {
-					pSendbuffer = 0;
-					mode = MODE_CHECKSUMME;
-				}
-			break;
-			case MODE_CHECKSUMME	
-				uint8_t sendbuffer[SENDBUFFER_SIZE_2] = {alle Daten zusammen gezaehlt};
-				if(pSendbuffer < SENDBUFFER_SIZE_2-1) {
-					pSendbuffer++;
-					mode = MODE_CHECKSUMME;
-				} else {
-					pSendbuffer = 0;
-					mode = MODE_IDLE;
-				}			
-	}	
+			}	
 }
+
+//void StateMachine(void *pvParameters){   //muss noch implementiert werden
+	//int mode = MODE_IDLE;
+	//for(;;){
+		//switch(mode)
+			//case MODE_IDLE:
+				//uint8_t sendbuffer[SENDBUFFER_SIZE_1] = {0,3};
+				//if (Button1 = 1 ) {
+					//mode = MODE_IDLE;
+						//if(pSendbuffer < SENDBUFFER_SIZE_1-1) {
+							//pSendbuffer++;
+						//} else {
+							//pSendbuffer = 0;
+						//}								
+				//}
+					//if (Button1 = 0) {
+					//mode = MODE_START;
+						//if(pSendbuffer < SENDBUFFER_SIZE_1-1) {
+							//pSendbuffer++;
+						//} else {
+							//pSendbuffer = 0;
+						//}						
+				//}
+			//break;
+			//case MODE_START:
+				//uint8_t sendbuffer[SENDBUFFER_SIZE_1] = {1,2};
+				//if(pSendbuffer < SENDBUFFER_SIZE_1-1) {
+					//pSendbuffer++;
+					//mode = MODE_START;
+				//} else {
+					//pSendbuffer = 0;
+					//mode = MODE_ID;
+				//}		
+			//break;			
+			//case MODE_ID:
+				//uint8_t sendbuffer[SENDBUFFER_SIZE_2] = {Daten, die im ID gesendet werden sollen. Sollen spaeter mal veraenderbar sein, um die Checksumme zu kontrollieren};
+				//if(pSendbuffer < SENDBUFFER_SIZE_2-1) {
+					//pSendbuffer++;
+					//mode = MODE_ID;
+				//} else {
+					//pSendbuffer = 0;
+					//mode = MODE_LENGHT;
+				//}
+			//break;
+			//case MODE_LENGHT
+				//uint8_t sendbuffer[SENDBUFFER_SIZE_2] = {0,0,2,3};
+				//if(pSendbuffer < SENDBUFFER_SIZE_2-1) {
+					//pSendbuffer++;
+					//mode = MODE_LENGHT;
+				//} else {
+					//pSendbuffer = 0;
+					//mode = MODE_DATA;
+				//}					
+			//case MODE_DATA:
+				//uint8_t sendbuffer[SENDBUFFER_SIZE_3] = {1,1,0,1, 1,3,1,1, 1,2,0,1, 1,2,3,1, 0,2,0,0, 1,1,0,0, 1,2,3,0, 1,3,1,1, 1,2,3,2, 1,2,1,0, 1,2,1,1, 1,3,0,2};	
+				//if(pSendbuffer < SENDBUFFER_SIZE_2-1) {
+					//pSendbuffer++;
+					//mode = MODE_DATA;
+				//} else {
+					//pSendbuffer = 0;
+					//mode = MODE_CHECKSUMME;
+				//}
+			//break;
+			//case MODE_CHECKSUMME	
+				//uint8_t sendbuffer[SENDBUFFER_SIZE_2] = {alle Daten zusammen gezaehlt};
+				//if(pSendbuffer < SENDBUFFER_SIZE_2-1) {
+					//pSendbuffer++;
+					//mode = MODE_CHECKSUMME;
+				//} else {
+					//pSendbuffer = 0;
+					//mode = MODE_IDLE;
+				//}			
+	//}	
+//}
 ISR(DMA_CH0_vect)
 {
 	//static signed BaseType_t test;
