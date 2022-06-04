2d8e0879b165ff816fadfa5d180d971d76f1fc56
lisagent <lisa.gent1@googlemail.com>
2022-06-04 16:25:27 +0200

    StateMashine FillBuffer überarbeitet

--- QAMDecGen/qamgen.c
+++ QAMDecGen/qamgen.c
@@ -24,7 +24,6 @@
 #include "qamgen.h"
 #include "ButtonHandler.h"
 
-//void Interface(void *pvParameters);
 
 EventGroupHandle_t EventGroupQAMGen;
 #define LOCK_DATA	1 << 0
@@ -60,29 +59,10 @@ const int16_t sinLookup50[NR_OF_SAMPLES*2] = {0x0,0xC8,0x187,0x238,0x2D3,0x353,0
 
 SemaphoreHandle_t MutexQAMGen; //A-Ressource
 uint8_t sendbuffer_IDLE[SENDBUFFER_SIZE_IDLE] = {0,3}; //P-Ressource
-uint8_t sendbuffer_SENDING[SENDBUFFER_SIZE_SENDING] = {0,1,0,1,0,1,2,1,3,0,1,1,3,2,1,0,0,1,0,1};
+//uint8_t sendbuffer_SENDING[SENDBUFFER_SIZE_SENDING] = {0,1,0,1,0,1,2,1,3,0,1,1,3,2,1,0,0,1,0,1};
 
-
-//Es soll eine StateMachine erstellt werden, mit deren Hilfe die einzelnen States ablaufen. 
-//Sie soll aus folgenden State bestehen: Idle, Start, ID,Lenght,Data,Checksumme
-//Stand 24.05.22 Die StateMAchine soll nicht so genau definiert sein. Es soll eine StateMachine im fillBuffer gemacht werden
-//Diese soll nur die zwei Zustände Idle und Sending haben. Im Sending werden alle Daten von Start,ID,Lengt,... zusammen geschrieben
-//Es muss noch einen Extratask haben, zb direkt im vQuamGen. Der besteht aus Taste und Encoder. Es kann die gleiche Tastenabfrage wie bei den anderen Projekten genommen werden
-//Wenn die Taste gedrückt wird, werden alle Daten durch den Encoder in den SendingBuffer geschrieben. Durch ein Semaphore soll dann noch sicher gestellt werden, dass die Daten
-//Nur im Idle-Zustand gesendet werden. Wenn er grade im Sending-State ist dürfen keine Daten übertragen werden.
-
-//Im fillBuffer muss der ganze Kram nochmal copy_paste gemacht werden für die zwei state. Es wird eine State-Maschine erstellt, mit der dann im fillBuffer zwischen
-//IDLE und SENDING gewechselt wird. Im IDLE kann unten das ++ so weg gemacht werden, dass er immer nur zwischen 0,3 toggelt.
-//Im SENDING soll es so gemacht werden wie im SENDBUFFER, mit der [] wird gesagt an welcher Stelle er welchen index haben soll. ALso zb als index [5] wird
-// das 5te SYmbol gesendet. Dan stelle [6] das sechste. Dabei ist es sehr wichtig zu defieren, wie lang das gesssamte senden ist
-// Mit einem MUTEX kann dann noch gesagt werden, dass er erst wieder Daten übernehmen darf, wenn er mit dem Senden fertig ist.
-
-void vQuamGen(void *pvParameters) {
-	
-	MutexQAMGen = xSemaphoreCreateMutex(); //Muss wahrscheinlich alle mit ins Main
-	//EventGroupQAMGen = xEventGroupCreate();
-	//xEventGroupSetBits(EventGroupQAMGen, ALGO1);
-	
+void vQuamGen(void *pvParameters) {	
+	MutexQAMGen = xSemaphoreCreateMutex(); 
 	while(evDMAState == NULL) {
 		vTaskDelay(3/portTICK_RATE_MS);
 	}
@@ -94,122 +74,101 @@ void vQuamGen(void *pvParameters) {
 			if(getButtonPress(BUTTON1) == SHORT_PRESSED) {
 				xEventGroupSetBits(EventGroupQAMGen, CREATE_SENDDATA);
 			}
-			
-			//sendbuffer[SENDBUFFER_SIZE_IDLE] = {0,3};
-			if((xEventGroupGetBits(EventGroupQAMGen) & LOCK_DATA) == LOCK_DATA) {
-				xEventGroupClearBits(EventGroupQAMGen, LOCK_DATA);
-				sendbuffer[SENDBUFFER_SIZE_IDLE] = 0;
-			}
-			if(xSemaphoreTake(MutexQAMGen, 10/portTICK_RATE_MS) == pdTRUE) {
-				sendbuffer[SENDBUFFER_SIZE_IDLE] = localx;
-				xSemaphoreGive(MutexQAMGen);
-			}
 			vTaskDelay(10/portTICK_RATE_MS);
-	}
-	//xTaskCreate(Interface, NULL, configMINIMAL_STACK_SIZE+500, NULL, 2, NULL);
-
-		}
-	}
 }
 
 void fillBuffer(uint16_t buffer[NR_OF_SAMPLES]) {
 	static int pSendbuffer_IDLE = 0;
 	static int pSendbuffer_SENDING = 0;
-	
-	for(int i = 0; i < NR_OF_SAMPLES;i++) {
-		switch(sendbuffer_IDLE[pSendbuffer_IDLE]) {
-			case 0:
-				buffer[i] = 0x800 + (sinLookup100[i]);
-			break;
-			case 1:
-				buffer[i] = 0x800 + (sinLookup100[i+16]);
-			break;
-			case 2:
-				buffer[i] = 0x800 + (sinLookup50[i]);
-			break;
-			case 3:
-				buffer[i] = 0x800 + (sinLookup50[i+16]);
-			break;
+		int mode = MODE_IDLE;
+			switch(){
+				case MODE_IDLE:
+					for(int i = 0; i < NR_OF_SAMPLES;i++) {
+						switch(sendbuffer_IDLE[pSendbuffer_IDLE]) {
+							case 0:
+								buffer[i] = 0x800 + (sinLookup100[i]);
+							break;
+							case 1:
+								buffer[i] = 0x800 + (sinLookup100[i+16]);
+							break;
+							case 2:
+								buffer[i] = 0x800 + (sinLookup50[i]);
+							break;
+							case 3:
+								buffer[i] = 0x800 + (sinLookup50[i+16]);
+							break;
+						}
+					}
+					if(pSendbuffer_IDLE < SENDBUFFER_SIZE_IDLE-1) {
+						pSendbuffer_IDLE++;
+					} else {
+						pSendbuffer_IDLE = 0;
+				}	
+				break;
+				
+				case MODE_SENDING:	
+					for(int i = 0; i < NR_OF_SAMPLES;i++) {
+						switch(sendbuffer_SENDING[pSendbuffer_SENDING]) {
+							case 0:
+								buffer[i] = 0x800 + (sinLookup100[i]);
+							break;
+							case 1:
+								buffer[i] = 0x800 + (sinLookup100[i+16]);
+							break;
+							case 2:
+								buffer[i] = 0x800 + (sinLookup50[i]);
+							break;
+							case 3:
+								buffer[i] = 0x800 + (sinLookup50[i+16]);
+							break;
+						}
+					}
+					if(pSendbuffer_SENDING < SENDBUFFER_SIZE_SENDING-1) {
+							pSendbuffer_SENDING++;
+						} else {
+							pSendbuffer_SENDING = 0;
+					}
+					break;
 		}
-	}
-	if(pSendbuffer_SENDING < SENDBUFFER_SIZE_IDLE-1) {
-		pSendbuffer_IDLE++;
-	} else {
-		pSendbuffer_IDLE = 0;
-	}	
-	
-	for(int i = 0; i < NR_OF_SAMPLES;i++) {
-		switch(sendbuffer_SENDING[pSendbuffer_SENDING]) {
-			case 0:
-				buffer[i] = 0x800 + (sinLookup100[i]);
-			break;
-			case 1:
-				buffer[i] = 0x800 + (sinLookup100[i+16]);
-			break;
-			case 2:
-				buffer[i] = 0x800 + (sinLookup50[i]);
-			break;
-			case 3:
-				buffer[i] = 0x800 + (sinLookup50[i+16]);
-			break;
-		}
-	}
-	if(pSendbuffer_SENDING < SENDBUFFER_SIZE_SENDING-1) {
-			pSendbuffer_SENDING++;
-		} else {
-			pSendbuffer_SENDING = 0;
-	}
 }
-//switch(mode)
-//case MODE_IDLE:
-//if(xSemaphoreTake(MutexQAMGen, 10/portTICK_RATE_MS) == pdFALSE) {
-	//uint8_t sendbuffer[SENDBUFFER_SIZE_IDLE] = {0,3};
-	//break;
-uint8_t Symbuffer[100]
+uint8_t sendbuffer_SENDING[100]
 uint8_t SendID[100]
+uint8_t checksum[20]
 void Create_Send_Data(){	
 		if((xEventGroupGetBits(EventGroupQAMGen)&StartTask)){		
-			case MODE_SENDING
-			if(xSemaphoreTake(MutexQAMGen, 10/portTICK_RATE_MS) == pdTRUE) {
-				SendID++;
-				char senddata[10] = "HelloWorld";
-				uint8_t datalen = strlen(senddata)
-				sendbuffer[0] =1
-				sendbuffer[1] =2
-				sendbuffer[2] =SendID & 0x03
-				sendbuffer[3] =(SendID >> 2 ) & 0x03
-				sendbuffer[4] =(senddata >> 0 ) & 0x03
-				sendbuffer[5] =(senddata >> 2 ) & 0x03
-				sendbuffer[6] =(senddata >> 4 ) & 0x03
-				sendbuffer[7] =(senddata >> 6 ) & 0x03
-				for (int i = 0; i<datalen; i++){
-					sendbuffer[8+i*4+0] = (senddata[i] >>0) & 0x03;
-					sendbuffer[8+i*4+1] = (senddata[i] >>2) & 0x03;
-					sendbuffer[8+i*4+2] = (senddata[i] >>3) & 0x03;
-					sendbuffer[8+i*4+3] = (senddata[i] >>4) & 0x03;
+			case MODE_SENDING;
+				if(xSemaphoreTake(MutexQAMGen, 10/portTICK_RATE_MS) == pdTRUE) {
+					SendID++;
+					char senddata[10] = "HelloWorld";
+					uint8_t datalen = strlen(senddata);
+					//Start-Signal
+					sendbuffer_SENDING[0] =1;
+					sendbuffer_SENDING[1] =2;
+					//ID
+					sendbuffer_SENDING[2] =SendID & 0x03;
+					sendbuffer_SENDING[3] =(SendID >> 2 ) & 0x03;
+					//Lenght
+					sendbuffer_SENDING[4] =(senddata >> 0 ) & 0x03;
+					sendbuffer_SENDING[5] =(senddata >> 2 ) & 0x03;
+					sendbuffer_SENDING[6] =(senddata >> 4 ) & 0x03;
+					sendbuffer_SENDING[7] =(senddata >> 6 ) & 0x03;
+					//Daten
+					for (int i = 0; i<datalen; i++){
+						sendbuffer_SENDING[8+i*4+0] = (senddata[i] >>0) & 0x03;
+						sendbuffer_SENDING[8+i*4+1] = (senddata[i] >>2) & 0x03;
+						sendbuffer_SENDING[8+i*4+2] = (senddata[i] >>3) & 0x03;
+						sendbuffer_SENDING[8+i*4+3] = (senddata[i] >>4) & 0x03;
+					}
+					//Checksumme
+					for(int i = 0; i<7+(datalen*4); i++) {
+						checksum += sendbuffer_SENDING[i];
+					}
+					sendbuffer_SENDING[(i<7+(datalen*4))+0] = checksum >> 0 &0x03;
+					sendbuffer_SENDING[(i<7+(datalen*4))+1] = checksum >> 2 &0x03;
+					sendbuffer_SENDING[(i<7+(datalen*4))+2] = checksum >> 4 &0x03;
+					sendbuffer_SENDING[(i<7+(datalen*4))+3] = checksum >> 6 &0x03;
+					xSemaphoreGive(MutexQAMGen);
 				}
-				for(int i = 0; i<7+(datalen*4); i++) {
-					checksum += sendbuffer[i];										
-				}
-				sendbuffer[(i<7+(datalen*4))+0] = checksum >> 0 &0x03;
-				sendbuffer[(i<7+(datalen*4))+1] = checksum >> 2 &0x03;
-				sendbuffer[(i<7+(datalen*4))+2] = checksum >> 4 &0x03;
-				sendbuffer[(i<7+(datalen*4))+3] = checksum >> 6 &0x03;
-				
-				
-				
-				sendbuffer[3] =1
-				uint8_t Symbuffer[100]
-					symbuffer [0} = 
-				
-				uint8_t Send_Start[2] = {1,2};
-				uint8_t Send_ID[2] = {0,0};
-				uint8_t SendLenght[4] = {0,0,2,3};
-				uint8_t SendData[32] = {1,1,0,1, 1,3,1,1, 1,2,0,1, 1,2,3,1, 0,2,0,0, 1,1,0,0, 1,2,3,0, 1,3,1,1, 1,2,3,2, 1,2,1,0, 1,2,1,1, 1,3,0,2};
-				uint8_t SendChecksumme[42] = Alle Daten zusammen
-				uint8_t sendbuffer[SENDBUFFER_SIZE_IDLE] = {1,2,ID,0,0,2,3, 1,1,0,1, 1,3,1,1, 1,2,0,1, 1,2,3,1, 0,2,0,0, 1,1,0,0, 1,2,3,0, 1,3,1,1, 1,2,3,2, 1,2,1,0, 1,2,1,1, 1,3,0,2, Checksumme};
-				xSemaphoreGive(MutexQAMGen);
-			}
 			break;
 		}
 
@@ -226,78 +185,3 @@ ISR(DMA_CH1_vect)
 	DMA.CH1.CTRLB|=0x10;
 	fillBuffer(&dacBuffer1[0]);
 }
-
-
-//void StateMachine(void *pvParameters){   //muss noch implementiert werden
-//	int mode = MODE_IDLE;
-//		for(;;){
-//			switch(mode)
-//				case MODE_IDLE:
-//					uint8_t sendbuffer[SENDBUFFER_SIZE_1] = {0,3};
-//					if (Button1 = 1 ) {
-//					mode = MODE_IDLE;
-//if(pSendbuffer < SENDBUFFER_SIZE_1-1) {
-//pSendbuffer++;
-//} else {
-//pSendbuffer = 0;
-//}
-//}
-//if (Button1 = 0) {
-//mode = MODE_START;
-//if(pSendbuffer < SENDBUFFER_SIZE_1-1) {
-//pSendbuffer++;
-//} else {
-//pSendbuffer = 0;
-//}
-//}
-//break;
-//case MODE_START:
-//uint8_t sendbuffer[SENDBUFFER_SIZE_1] = {1,2};
-//if(pSendbuffer < SENDBUFFER_SIZE_1-1) {
-//pSendbuffer++;
-//mode = MODE_START;
-//} else {
-//pSendbuffer = 0;
-//mode = MODE_ID;
-//}
-//break;
-//case MODE_ID:
-//uint8_t sendbuffer[SENDBUFFER_SIZE_2] = {Daten, die im ID gesendet werden sollen. Sollen spaeter mal veraenderbar sein, um die Checksumme zu kontrollieren};
-//if(pSendbuffer < SENDBUFFER_SIZE_2-1) {
-//pSendbuffer++;
-//mode = MODE_ID;
-//} else {
-//pSendbuffer = 0;
-//mode = MODE_LENGHT;
-//}
-//break;
-//case MODE_LENGHT
-//uint8_t sendbuffer[SENDBUFFER_SIZE_2] = {0,0,2,3};
-//if(pSendbuffer < SENDBUFFER_SIZE_2-1) {
-//pSendbuffer++;
-//mode = MODE_LENGHT;
-//} else {
-//pSendbuffer = 0;
-//mode = MODE_DATA;
-//}
-//case MODE_DATA:
-//uint8_t sendbuffer[SENDBUFFER_SIZE_3] = {1,1,0,1, 1,3,1,1, 1,2,0,1, 1,2,3,1, 0,2,0,0, 1,1,0,0, 1,2,3,0, 1,3,1,1, 1,2,3,2, 1,2,1,0, 1,2,1,1, 1,3,0,2};
-//if(pSendbuffer < SENDBUFFER_SIZE_2-1) {
-//pSendbuffer++;
-//mode = MODE_DATA;
-//} else {
-//pSendbuffer = 0;
-//mode = MODE_CHECKSUMME;
-//}
-//break;
-//case MODE_CHECKSUMME
-//uint8_t sendbuffer[SENDBUFFER_SIZE_2] = {alle Daten zusammen gezaehlt};
-//if(pSendbuffer < SENDBUFFER_SIZE_2-1) {
-//pSendbuffer++;
-//mode = MODE_CHECKSUMME;
-//} else {
-//pSendbuffer = 0;
-//mode = MODE_IDLE;
-//}
-//}
-//}
